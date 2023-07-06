00beaa396be5e4d4395f013b310b98083de884cb
Dhananjai Sharma <dhananjaisharma09@gmail.com>
2023-07-06 18:53:52 +0530

    minor fixes

--- task/src/components/MultiStep/validationSchemas/stepFourSchema.ts
+++ task/src/components/MultiStep/validationSchemas/stepFourSchema.ts
@@ -2,11 +2,15 @@ import { z } from 'zod';
 
 const FILE_SIZE_LIMIT = 10 * 1024 * 1024; // 10MB file size limit
 const ACCEPTED_TYPES = ["image/png", "application/pdf"];
+const ALLOWED_FILE_TYPE = 'image/png'; 
 
 export const stepFourSchema = z.object({
   multi_ups1: z.object({
       name: z.string().nonempty('File is required'),
-      type: z.string().nonempty('File type is required'),
+      type: z.string().nonempty('File type is required').refine(value => value === ALLOWED_FILE_TYPE, {
+        message: 'File type must be PNG',
+        path: ['type'],
+      }),
       size: z.number().min(1, 'File size must be greater than 0').max(FILE_SIZE_LIMIT, 'File size exceeds limit'),
     })
     .refine(
